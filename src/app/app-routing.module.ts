68390356e62b238a9472aef20c093461689fd36c
Riya-Gupta-1999 <riyagupta23041999@gmail.com>
2024-09-25 18:30:14 +0530

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/component/login/login.component';
 import { AllBooksComponent } from './public/component/all-books/all-books.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 const routes: Routes = [
    {path: '', redirectTo:'login',pathMatch:'full' },
    {path: 'allbooks', component: AllBooksComponent },
    {path: 'login', component: LoginComponent },
+   {path: 'auth', component: AuthComponent, loadChildren: () =>import('./auth/auth.module').then(x => x.AuthModule)},
    {path: '**', component: NotFoundComponent }
 ];
 
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from '../app-routing.module';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
-import { AuthComponent } from './auth.component';
 import { ChangePasswordComponent } from './component/change-password/change-password.component';
 import { SignupComponent } from './component/signup/signup.component';
 
 const routes: Routes = [
-  {path: 'auth', component: AuthComponent,
-    children:[
-      {path: '', redirectTo:'login',pathMatch:'full' },
+  // {path: 'auth', component: AuthComponent,
+  //   children:[
+      // {path: '', redirectTo:'login',pathMatch:'full' },
       {path: 'login', component: LoginComponent},
       {path: 'change-password', component: ChangePasswordComponent},
       {path: 'signup', component: SignupComponent},
-    ]
-   }
+  //   ]
+  //  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AuthRoutingModule { }
